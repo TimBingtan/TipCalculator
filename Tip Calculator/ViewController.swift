ba79eda16f3ad2b5e9ff9d98f11c37255924a2ac
Tim Bingtan <elevntwnty@gmail.com>
2019-01-03 12:30:38 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Tim Bingtan on 1/3/19.
+//  Copyright © 2019 Tim Bingtan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
